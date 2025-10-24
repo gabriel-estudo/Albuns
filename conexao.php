6f12ae3b821a3c7735bd846d995424fd4764e3c9
gabriel-estudo <milgabriel552@gmail.com>
2025-10-24 13:55:00 -0300

    alteracao

--- conexao.php
+++ conexao.php
@@ -1,10 +1,16 @@
 <?php
 
-// Conexão com o banco (MySQL)
-$host = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "albuns";
+// Conexão com o banco (MySQL)  DESENVOLVIMENTO
+// $host = "localhost";
+// $usuario = "root";
+// $senha = "";
+// $banco = "albuns";
+
+// Conexão com o banco (MySQL)  PRODUÇÃO
+$host = "www.gabrielmil.com.br";
+$usuario = "gabrielmil";
+$senha = "Gabriel@230107";
+$banco = "gabrielmil01";
 
 $conexao = new mysqli($host, $usuario, $senha, $banco);
 
